2a66e6bb0d123aac9bd3bca3a2be0abb84459961
auxten <pengcheng.wang@thunderdb.io>
2019-04-18 09:44:27 +0800

    Remove dependency of make, add meaningful test

--- cql.rb
+++ cql.rb
@@ -6,18 +6,25 @@ class Cql < Formula
   head "https://github.com/CovenantSQL/CovenantSQL.git"
 
   depends_on "go" => :build
-  depends_on "make" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     ENV["CQLVERSION"] = "v0.5.0"
+    ENV["CGO_ENABLED"] = "1"
     mkdir_p "src/github.com/CovenantSQL"
+    ldflags = "-X main.version=v0.5.0 " \
+      "-X github.com/CovenantSQL/CovenantSQL/conf.RoleTag=C " \
+      "-X github.com/CovenantSQL/CovenantSQL/utils/log.SimpleLog=Y"
     ln_s buildpath, "src/github.com/CovenantSQL/CovenantSQL"
-    system "make", "bin/cql"
+    system "go", "build", "-tags", "sqlite_omit_load_extension",
+      "-ldflags", ldflags, "-o", "bin/cql", "github.com/CovenantSQL/CovenantSQL/cmd/cql"
     bin.install "bin/cql"
   end
 
   test do
-    system bin/"cql", "help"
+    testconf = testpath/"confgen"
+    system bin/"cql", "generate", "-config", testconf, "-no-password", "config"
+    assert_predicate testconf/"private.key", :exist?
+    assert_predicate testconf/"config.yaml", :exist?
   end
 end
